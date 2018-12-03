079e6c0de3bed15671b4c065fc52f1868a6fe231
denis <dkarpelevich@gmail.com>
2018-12-03 17:27:38 +0100

    2 more tasks

---
+++ count_vowels.py
@@ -0,0 +1,20 @@
+vowels = ['a', 'e', 'y', 'u', 'i', 'o']
+
+
+def count_vowels(string):
+    vowel_amount = 0
+    dict_with_vowels = {}
+    for j in vowels:
+        each_vowel = 0
+        for i in string.lower():
+            if i == j:
+                each_vowel += 1
+                dict_with_vowels.update({str(j): each_vowel})
+            else:
+                dict_with_vowels.update({str(j): each_vowel})
+        vowel_amount += each_vowel
+    print('Vowel {0}'.format(dict_with_vowels))
+    print('Amount of vowels is {0}'.format(vowel_amount))
+
+
+count_vowels('AiIeEjeYyYyooooooooo  ')
---
+++ reverse_a_string.py
@@ -0,0 +1,5 @@
+def reverse_string(string):
+    print(string[::-1])
+
+
+reverse_string('That a string')
